954c3a586a3538d95579b6157ed6c052770f9016
andregoudinho <andre_goudinho@estudante.sesisenai.org.br>
2026-03-24 12:04:56 -0300

    add - ex05

---
+++ scripts/ex05.js
@@ -0,0 +1,12 @@
+function ex05(){
+
+    let numero = parseInt(prompt("Digite um número inteiro:"));
+
+    if (numero %2==0){
+        numero++;
+        console.log("O número era par e foi transformado em ímpar: ", numero)
+    } else {
+        numero = numero -1;
+        console.log("O número era ímpar e foi tranformado em par: ", numero)
+    }
+}
