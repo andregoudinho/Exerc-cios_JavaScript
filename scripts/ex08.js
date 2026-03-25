ef9033f1c73af7edb7b2ce9f0e565536aa726669
andre_goudinho <andre_goudinho@estudante.sesisenai.org.br>
2026-03-24 21:15:55 -0300

    add - ex08

---
+++ scripts/ex08.js
@@ -0,0 +1,15 @@
+function ex08() {
+    let x = 10;
+    let y = 2;
+
+    let diferenca = x - y;
+
+    let resultado = (2 * x) + (3 * y);
+
+    let multiplicacao = x * y;
+
+    alert("A diferença das duas variáveis é: " + diferenca);
+    alert("O dobro da primeira e o triplo da segunda é: " + resultado);
+    alert("A multiplicação entre as duas é: " + multiplicacao);
+
+}
